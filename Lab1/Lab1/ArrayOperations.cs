e83d044b710eabfb57d7f7ca27f2bbcca2d5655d
Hanna Shymanska <a.shymanska.ip13@kpi.ua>
2022-04-29 02:15:39 +0300

    First lab done

---
+++ Lab1/Lab1/ArrayOperations.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ArrayOperations
+{
+    public class ArrayGenerator
+    {
+        public int[] GetArray(int length, bool? sorted = null)
+        {
+            int[] array = new int[length];
+            Random rand = new Random();
+            for (int i = 0; i < length; i++)
+            {
+                if (sorted is null)
+                    array[i] = rand.Next(-5000, 10001);
+                else if (sorted is true)
+                {
+                    array[i] = i;
+                } 
+                else
+                {
+                    array[i] = length - i;
+                }
+            }
+
+            return array;
+        }
+    }
+    public class BubbleSort
+    {
+        private int _comparisonAmount;
+        private int _swapAmount;
+        
+        protected void Swap(int[] array, int step, int upperBound)
+        {
+            for (int j = 0; j < array.Length - upperBound; j++)
+            {
+                _comparisonAmount++;
+                if (array[j + step] < array[j])
+                {
+                    (array[j], array[j + step]) = (array[j + step], array[j]);
+                    _swapAmount++;
+                }
+            }
+        }
+
+        public void DisplayCharacteristics()
+        {
+            Console.WriteLine("The amount of comparisons: " + _comparisonAmount);
+            Console.WriteLine("The amount of swaps: " + _swapAmount);
+        }
+        
+        public void DisplayArray(int[] array)
+        {
+            Console.WriteLine($"\n{this.GetType().Name}");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write("{0,7}", array[i]);
+            }
+            Console.WriteLine();
+        }
+
+        public double GetSortingTime(int[] array)
+        {
+            Stopwatch sw = Stopwatch.StartNew();
+            DoSort(array);
+            sw.Stop();
+            return sw.Elapsed.TotalMilliseconds;
+        }
+        
+        protected virtual void DoSort(int[] array)
+        {
+            for (int i = 1; i <= array.Length - 1; i++)
+            {
+                Swap(array, 1, i);
+            }
+        }
+    }
+
+    public class CombSort : BubbleSort
+    {
+        private int GetUpdatedGap(int gap)
+        {
+            gap = (int)(gap / 1.2473);
+            if (gap < 1) gap = 1;
+            return gap;
+        }
+
+        protected override void DoSort(int[] array)
+        {
+            int gap = array.Length;
+            while (gap != 1)
+            {
+                gap = GetUpdatedGap(gap);
+                Swap(array, gap, gap);
+            }
+        }
+    }
+}
---
+++ Lab1/Lab1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using ArrayOperations;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array;
+            int length;
+            
+            BubbleSort bubbleSort = new BubbleSort();
+            CombSort combSort = new CombSort();
+            ArrayGenerator arrGen = new ArrayGenerator();
+
+            length = GetLength();
+            array = arrGen.GetArray(length);
+            
+            Console.WriteLine("\nInitial array is: ");
+            bubbleSort.DisplayArray(array);
+            Console.WriteLine("Execution time in ms "  + bubbleSort.GetSortingTime(array));
+            Console.WriteLine("\nSorted array is " );
+            bubbleSort.DisplayArray(array);
+            bubbleSort.DisplayCharacteristics();
+            
+            length = GetLength();
+            array = arrGen.GetArray(length);
+            Console.WriteLine("\nInitial array is: ");
+            combSort.DisplayArray(array);
+            Console.WriteLine("Execution time in ms " + combSort.GetSortingTime(array));
+            Console.WriteLine("\nSorted array is " );
+            combSort.DisplayArray(array);
+            combSort.DisplayCharacteristics();
+        }
+        public static int GetLength()
+        {
+            int length = 0;
+            Console.WriteLine("Enter the length of array to generate: ");
+            length = Convert.ToInt32(Console.ReadLine());
+            return length;
+        }
+    }
+    
+    
+}
